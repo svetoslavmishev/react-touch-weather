1c96e7b0a7f2d5dea3b47ad65d9c9a6a0ffb8260
svetoslavmishev <svetlioo.m@gmail.com>
2018-10-31 06:52:33 +0200

    input field location validation

--- src/components/Forecast/ForecastPredictions.jsx
+++ src/components/Forecast/ForecastPredictions.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import { calcTime, calcDayOfWeek } from '../../utils/dateFormatting';
+import { calcDayOfWeek } from '../../utils/dateFormatting';
 
 
 export default class ForecastPredictions extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
+    const { icon, maxTemp, day, description } = this.props;
     return (
       <div className="forecast">
         <div className="forecast-header">
-          <div className="day">Monday</div>
+          <div className="day">{calcDayOfWeek(day)}</div>
         </div>
         <div className="forecast-content">
           <div className="forecast-icon">
-            <img src="images/icons/icon-3.svg" alt="" width="48" />
+            <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="weather" width="50" />
           </div>
-          <div className="degree">23<sup>o</sup>C</div>
-          <small>18<sup>o</sup></small>
+          <small><sup>{description.toUpperCase()}</sup></small>
+          <div className="degree">{maxTemp}<sup>o</sup>C</div>
         </div>
       </div>
     )
