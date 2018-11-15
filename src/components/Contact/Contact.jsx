2062848511bcf343b8dd6109e202e6264c3482e5
svetoslavmishev <svetlioo.m@gmail.com>
2018-11-15 20:39:44 +0200

    add contacts

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <h1 className="contact">Hello from Contact!</h1>
+    <div class="fullwidth-block">
+      <div class="container">
+        <div class="d-flex justify-content-start">
+          <div class="contact-details">
+            <div class="contact-info">
+              <p><a href="#"><img src="images/icon-marker.png" alt="" /> Sofia, Bulgaria</a> </p>
+              <p><a href="#"><img src="images/icon-envelope.png" alt="" />svetlioo.m@gmail.com</a> </p>
+              <p><a href="#"><img src="images/icon-phone.png" alt="" />+359 123456789</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
